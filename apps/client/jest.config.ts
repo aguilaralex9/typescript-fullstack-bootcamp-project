e6e62a980dc9515e6d980c1aa2f329c7593a97e5
Alexander Aguilar <aguilar.rodrigo9a@gmail.com>
2024-09-20 22:51:25 -0600

    chore: add more configs for client side

--- apps/client/jest.config.ts
+++ apps/client/jest.config.ts
@@ -1,7 +1,12 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
   verbose: true,
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', 
+    '^.+\\.(js|jsx)$': 'babel-jest', 
+  }
 };
 
 export default config;
